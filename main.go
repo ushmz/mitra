85e3dceefca4c5a08bc8f34047461ba618d48eda
ushmz <44394399+ushmz@users.noreply.github.com>
2022-12-07 23:28:49 +0900

    feat: update middlewares

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"mitra/auth"
 	"mitra/config"
 	"mitra/handler"
+	"mitra/store"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -16,20 +18,34 @@ import (
 var gendoc = flag.Bool("docgen", false, "Generate router documentation")
 
 func main() {
-	if err := config.Init(); err != nil {
+	err := config.Init()
+	if err != nil {
 		msg := fmt.Sprintf("Failed to load configurations\n%v", err)
 		panic(msg)
 	}
 
+	db, err := store.InitDB()
+	if err != nil {
+		msg := fmt.Sprintf("Failed to connect DB\n%v", err)
+		panic(msg)
+	}
+
+	fb, err := auth.InitFirebaseApp()
+	if err != nil {
+		msg := fmt.Sprintf("Failed to initialize firebase SDK\n%v", err)
+		panic(msg)
+	}
+
 	r := chi.NewRouter()
-	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(render.SetContentType(render.ContentTypeJSON))
+	r.Use(corsHandler())
 
 	r.Get("/", handler.Index)
 
-	r.Mount("/api", router())
-	r.Mount("/admin", adminRouter())
+	r.Mount("/api/u", router(db, fb))
+	r.Mount("/api/a", authRouter(db, fb))
+	// r.Mount("/api/admin", adminRouter())
 
 	flag.Parse()
 	if *gendoc {
--- middleware.go
+++ middleware.go
@@ -1,14 +1,68 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"strings"
 
-func adminOnly(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		isAdmin, ok := r.Context().Value("acl.admin").(bool)
-		if !ok || !isAdmin {
-			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
+	"net/http"
+
+	firebase "firebase.google.com/go"
+	"github.com/rs/cors"
+)
+
+var (
+	app firebase.App
+)
+
+func corsHandler() func(http.Handler) http.Handler {
+	opt := cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders:   []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+	}
+	handler := cors.New(opt).Handler
+	return handler
+}
+
+func adminOnly() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			isAdmin, ok := r.Context().Value("acl.admin").(bool)
+			if !ok || !isAdmin {
+				http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+// firebaseAuth : Auth middleware that check "Authorization" header and verify token
+func firebaseAuth(app *firebase.App) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			if ctx == nil {
+				ctx = context.Background()
+			}
+
+			client, err := app.Auth(ctx)
+			if err != nil {
+				// errors.New("Failed to get auth client")
+				http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+				return
+			}
+
+			auth := r.Header.Get("Authorization")
+			idToken := strings.Replace(auth, "Bearer ", "", 1)
+
+			if _, err = client.VerifyIDTokenAndCheckRevoked(ctx, idToken); err != nil {
+				http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
 }
